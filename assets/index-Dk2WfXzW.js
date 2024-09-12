fce16e55764090d03358bf336bb4698b2208e837
wanli <3203045385@qq.com>
2024-09-12 10:00:48 +0800

    edit 更新

--- assets/index-Dk2WfXzW.js
+++
@@ -1,1 +0,0 @@
-import{o as e,b as o,h as t}from"./index-0MJCgTmw.js";const n={id:"app"},r={__name:"index",setup(a){return e(()=>{}),(s,_)=>(o(),t("div",n," 我的资料 "))}};export{r as default};
---
+++ dist/assets/index-B6b3ELOc.js
@@ -0,0 +1,1 @@
+import{o as e,b as o,h as t}from"./index-CVI66faI.js";const n={id:"app"},r={__name:"index",setup(a){return e(()=>{}),(s,_)=>(o(),t("div",n," 模板管理 "))}};export{r as default};
